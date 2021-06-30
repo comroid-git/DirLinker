c51eee4022052ee54b38d8b1d60d64508e494b6b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-06-30 14:06:48 +0200

    add Configuration popup

---
+++ DirLinkerWPF/ConfigPopup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DirLinkerConfig;
+
+namespace DirLinkerWPF
+{
+    /// <summary>
+    /// Interaction logic for ConfigPopup.xaml
+    /// </summary>
+    public partial class ConfigPopup : Window
+    {
+        private readonly DirLinker _main;
+
+        public ConfigPopup(DirLinker main)
+        {
+            _main = main;
+            InitializeComponent();
+        }
+
+        private void SaveAndOpen(object sender, RoutedEventArgs e)
+        {
+            _main.CleanupConfig();
+            Configuration.SaveConfig();
+            Process.Start("explorer.exe", Configuration.ConfigFile);
+        }
+    }
+}
